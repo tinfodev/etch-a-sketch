dc8bd710dfea1238073b39766234608a33d096c7
tinfodev <tinfo.dev@gmail.com>
2025-02-19 19:24:04 -0700

    Add drawing logic

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // global variables here 
 const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_USER_COLOR = "000000";
 
-// querySelector stuff goes here
+let currentTool = 'pencil';
+let userColor = "";
+
 const drawingBoard = document.querySelector('.drawing-board');
 
 function createGrid(gridSize) {
@@ -11,10 +14,27 @@ function createGrid(gridSize) {
         for (let j = 0; j < gridSize; j++) {
             const col = document.createElement('div');
             col.classList.add('column');
+            col.setAttribute('data-opacity', 0);
             row.appendChild(col);
         }
         drawingBoard.appendChild(row);
     }
 }
 
+// move to bottom?
 createGrid(DEFAULT_GRID_SIZE);
+
+const grids = Array.from(document.querySelectorAll('.column'));
+
+for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener('mouseover', (evt) => {
+        if (currentTool === 'eraser') return;
+
+        const currentElement = evt.target;
+
+        let currentOpacity = Number(currentElement.getAttribute('data-opacity'));
+        const incrementOpacity = currentOpacity <= 1 ? currentOpacity += 0.1 : currentOpacity;
+        currentElement.style.opacity = `${incrementOpacity}`;
+        currentElement.setAttribute('data-opacity', incrementOpacity);
+    })
+}
